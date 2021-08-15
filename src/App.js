06b426644b5c2f0aa41f57154dcac44c142e0f3c
naima-shk <naimashaikh550@gmail.com>
2021-08-15 12:02:23 +0500

    'implement-react'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ListItem from './Component/ListItem/ListItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [nameState, setNameState] = useState('naima')
+    return ( <
+        div className = 'App' >
+        <
+        header className = 'App-header' >
+        <
+        button onClick = {
+            () => setNameState('shaikh')
+        } > Update name < /button>
 
+        <
+        div > My name is { nameState } < /div>  <
+        div > { nameState } < /div> <
+        ListItem name = { nameState }
+        /> < /
+        header > < /
+        div >
+    );
+}
 export default App;
---
+++ src/Component/ListItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ListItem(props) {
+    return ( <
+        div >
+        <
+        div > parent comp { props.name } < /div> <
+        /div>
+    )
+}
+export default ListItem;
